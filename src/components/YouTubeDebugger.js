74c89ff0b3fa9a20f0d52029040ed81c9756c706
TheNanoBay <noorani.mehdi@outlook.com>
2021-02-25 01:15:02 -0600

    commit tree 3c78e09a3c8253362a183fde5bc2e27f1b3ab326

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+
+    constructor(){
+        super()
+        this.state={
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        }
+          
+    }
+
+    handleClick = () =>{
+        this.setState(previousState =>{
+            return{
+                settings:{
+                    ...this.state.settings,
+                    bitrate: 12
+                }
+            }
+        })
+    }
+
+    handleResolution = () =>{
+        console.log(this.state)
+        this.setState({
+            settings:{
+                ...this.state.settings,
+                video:{
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                <button className="bitrate" onClick={this.handleClick}>bitrate</button>
+                <button className="resolution" onClick={this.handleResolution}>resolution</button>
+            </div>
+        )
+    }
+}
